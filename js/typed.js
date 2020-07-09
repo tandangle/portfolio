d5687effce1e6a4db56fb81bf4ece07ebf5b137c
Tandangle <tandangle76@gmail.com>
2020-07-09 10:19:44 -0400

    initial commit

---
+++ js/typed.js
@@ -0,0 +1,14 @@
+var options = {
+	strings: [ 'full stack web developer', 'foodie', 'dog dad', 'geek' ],
+	loop: true,
+	startDelay: 1750,
+	typeSpeed: 50,
+	backSpeed: 50,
+	onBegin: function() {
+		options.startDelay = 500;
+	}
+};
+
+var typed = new Typed(document.getElementById('typed'), options);
+
+function setDelay(self) {}
